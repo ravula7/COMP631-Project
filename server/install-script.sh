31691fc8c4f5a9ba7af5b01d2c2bbca651c3bc3d
Sheel Patel <sheel@letswhirl.com>
2023-03-21 23:21:24 -0400

    test header and updated script

--- server/install-script.sh
+++ server/install-script.sh
@@ -7,3 +7,7 @@ sudo cp -f -r webpage /webpage
 sudo adduser --system --no-create-home --shell /sbin/nologin --group nginx
 sudo systemctl start nginx
 sudo systemctl enable nginx
+sudo apt install snapd
+sudo snap install core
+sudo snap install --classic certbot
+sudo certbot --nginx
