518f77b8dd7b2a7ea70da9521ac4c3af0679c4fc
Sheel Patel <sheel@letswhirl.com>
2023-03-21 22:54:51 -0400

    updated install script

--- server/install-script.sh
+++ server/install-script.sh
@@ -3,6 +3,6 @@
 sudo apt update
 sudo apt-get install -y nginx
 cp -f nginx.conf /etc/nginx/nginx.conf
-cp -f -r /webpage /webpage
-systemctl start nginx
-systemctl enable nginx
+cp -f -r webpage /webpage
+sudo systemctl start nginx
+sudo systemctl enable nginx
