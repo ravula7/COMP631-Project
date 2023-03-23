a5c2ad4269641b087964fd55e1e933871654967d
ravula7 <69436086+ravula7@users.noreply.github.com>
2023-03-22 20:36:12 -0400

    Update getscript.js

--- client/getscript.js
+++ client/getscript.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 
 async function getMP4() {
     try {
-        const fetched = await fetch('https://stackoverflow.com/questions/3155960/is-it-possible-to-retrieve-the-ms-latency-from-a-get-request-javascript');//http://146.148.34.33/');
+        const fetched = await fetch('http://vm1.research.letswhirl.com/');//http://146.148.34.33/');
 
         if (!fetched.ok) {
             throw new Error(`Error! status: ${fetched.status}`);
