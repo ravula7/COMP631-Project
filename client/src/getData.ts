abef3177c1061d2c8b8ba0ac8c3d4305f724c65e
Sheel Patel <sheel@letswhirl.com>
2023-04-24 17:36:00 -0400

    add ignoreSSL

--- client/src/getData.ts
+++ client/src/getData.ts
@@ -48,6 +48,7 @@ export const getData = async ({
     const browser = await firefox.launch()
     const context = await browser.newContext({
         userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36 Edg/96.0.1054.29+HTTP/3',
+        ignoreHTTPSErrors: true
     });
     const page = await context.newPage()
 
