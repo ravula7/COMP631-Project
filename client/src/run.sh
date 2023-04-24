303d4104c793f9ab66c0f1059b9acb7083289da6
Sheel Patel <sheel@letswhirl.com>
2023-04-23 23:44:37 -0400

    fix script

--- client/src/run.sh
+++ client/src/run.sh
@@ -1,23 +1,23 @@
 cd ..
 tc qdisc del dev eth0 root netem
-./dist/client/src/measure-all-protocols.sh --loss-rate 0
+./dist/client/src/measure-all-protocols.js --loss-rate 0
 tc qdisc add dev eth0 root netem loss 0.1%
-./dist/client/src/measure-all-protocols.sh --loss-rate 0.1
+./dist/client/src/measure-all-protocols.js --loss-rate 0.1
 tc qdisc add dev eth0 root netem loss 0.5%
-./dist/client/src/measure-all-protocols.sh --loss-rate 0.5
+./dist/client/src/measure-all-protocols.js --loss-rate 0.5
 tc qdisc add dev eth0 root netem loss 1%
-./dist/client/src/measure-all-protocols.sh --loss-rate 1
+./dist/client/src/measure-all-protocols.js --loss-rate 1
 tc qdisc add dev eth0 root netem loss 2%
-./dist/client/src/measure-all-protocols.sh --loss-rate 2
+./dist/client/src/measure-all-protocols.js --loss-rate 2
 tc qdisc add dev eth0 root netem loss 5%
-./dist/client/src/measure-all-protocols.sh --loss-rate 5
+./dist/client/src/measure-all-protocols.js --loss-rate 5
 tc qdisc add dev eth0 root netem loss 10%
-./dist/client/src/measure-all-protocols.sh --loss-rate 10
+./dist/client/src/measure-all-protocols.js --loss-rate 10
 tc qdisc add dev eth0 root netem loss 25%
-./dist/client/src/measure-all-protocols.sh --loss-rate 25
+./dist/client/src/measure-all-protocols.js --loss-rate 25
 tc qdisc add dev eth0 root netem loss 50%
-./dist/client/src/measure-all-protocols.sh --loss-rate 50
+./dist/client/src/measure-all-protocols.js --loss-rate 50
 tc qdisc add dev eth0 root netem loss 75%
-./dist/client/src/measure-all-protocols.sh --loss-rate 75
+./dist/client/src/measure-all-protocols.js --loss-rate 75
 tc qdisc add dev eth0 root netem loss 90%
-./dist/client/src/measure-all-protocols.sh --loss-rate 90
+./dist/client/src/measure-all-protocols.js --loss-rate 90
