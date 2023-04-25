27d9ceba5851dc8a9a558a76c6698a9e5c4fd2e7
Sheel Patel <sheel@letswhirl.com>
2023-04-24 20:47:59 -0400

    add results logging for media

--- client/src/run.sh
+++ client/src/run.sh
@@ -1,55 +1,55 @@
 cd ..
 tc qdisc del dev eth0 root netem
 echo "Running with no loss"
-node ./dist/client/src/measure-all-protocols.js --loss-rate 0
+node ./dist/client/src/measure-all-protocols.js --loss-rate 0 --log-results -m
 echo "SUCCESS"
 tc qdisc del dev eth0 root netem
 tc qdisc add dev eth0 root netem loss 0.1%
 echo "Running with 0.1% loss"
-node ./dist/client/src/measure-all-protocols.js --loss-rate 0.1
+node ./dist/client/src/measure-all-protocols.js --loss-rate 0.1 --log-results -m
 echo "SUCCESS"
 tc qdisc del dev eth0 root netem
 tc qdisc add dev eth0 root netem loss 0.5%
 echo "Running with 0.5% loss"
-node ./dist/client/src/measure-all-protocols.js --loss-rate 0.5
+node ./dist/client/src/measure-all-protocols.js --loss-rate 0.5 --log-results -m
 echo "SUCCESS"
 tc qdisc del dev eth0 root netem
 tc qdisc add dev eth0 root netem loss 1%
 echo "Running with 1% loss"
-node ./dist/client/src/measure-all-protocols.js --loss-rate 1
+node ./dist/client/src/measure-all-protocols.js --loss-rate 1 --log-results -m
 echo "SUCCESS"
 tc qdisc del dev eth0 root netem
 tc qdisc add dev eth0 root netem loss 2%
 echo "Running with 2% loss"
-node ./dist/client/src/measure-all-protocols.js --loss-rate 2
+node ./dist/client/src/measure-all-protocols.js --loss-rate 2 --log-results -m
 echo "SUCCESS"
 tc qdisc del dev eth0 root netem
 tc qdisc add dev eth0 root netem loss 5%
 echo "Running with 5% loss"
-node ./dist/client/src/measure-all-protocols.js --loss-rate 5
+node ./dist/client/src/measure-all-protocols.js --loss-rate 5 --log-results -m
 echo "SUCCESS"
 tc qdisc del dev eth0 root netem
 tc qdisc add dev eth0 root netem loss 10%
 echo "Running with 10% loss"
-node ./dist/client/src/measure-all-protocols.js --loss-rate 10
+node ./dist/client/src/measure-all-protocols.js --loss-rate 10 --log-results -m
 echo "SUCCESS"
 tc qdisc del dev eth0 root netem
 tc qdisc add dev eth0 root netem loss 25%
 echo "Running with 25% loss"
-node ./dist/client/src/measure-all-protocols.js --loss-rate 25
+node ./dist/client/src/measure-all-protocols.js --loss-rate 25 --log-results -m
 echo "SUCCESS"
 tc qdisc del dev eth0 root netem
 tc qdisc add dev eth0 root netem loss 50%
 echo "Running with 50% loss"
-node ./dist/client/src/measure-all-protocols.js --loss-rate 50
+node ./dist/client/src/measure-all-protocols.js --loss-rate 50 --log-results -m
 echo "SUCCESS"
 tc qdisc del dev eth0 root netem
 tc qdisc add dev eth0 root netem loss 75%
 echo "Running with 75% loss"
-node ./dist/client/src/measure-all-protocols.js --loss-rate 75
+node ./dist/client/src/measure-all-protocols.js --loss-rate 75 --log-results -m
 echo "SUCCESS"
 tc qdisc del dev eth0 root netem
 tc qdisc add dev eth0 root netem loss 90%
 echo "Running with 90% loss"
-node ./dist/client/src/measure-all-protocols.js --loss-rate 90
+node ./dist/client/src/measure-all-protocols.js --loss-rate 90 --log-results -m
 echo "SUCCESS"
