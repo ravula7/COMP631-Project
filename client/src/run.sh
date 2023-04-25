71870b556eac4ada44a60fc172d829f161116dcb
Sheel Patel <sheel@letswhirl.com>
2023-04-24 23:29:08 -0400

    updated run script

--- client/src/run.sh
+++ client/src/run.sh
@@ -8,7 +8,7 @@ tc qdisc del dev eth0 root netem
 
 # Run the script with no loss
 echo "Running with no loss"
-node ./dist/client/src/measure-all-protocols.js --loss-rate 0 "$@"
+node ./dist/src/measure-all-protocols.js --loss-rate 0 "$@"
 echo "SUCCESS"
 
 # Set up network emulation for each loss rate and run the script
@@ -17,7 +17,7 @@ for loss_rate in 0.1 0.5 1 2 5 10 25 50 75 90; do
   tc qdisc add dev eth0 root netem loss "$loss_rate"% 
 
   echo "Running with $loss_rate% loss"
-  node ./dist/client/src/measure-all-protocols.js --loss-rate "$loss_rate" "$@"
+  node ./dist/src/measure-all-protocols.js --loss-rate "$loss_rate" "$@"
   echo "SUCCESS"
 done
 
